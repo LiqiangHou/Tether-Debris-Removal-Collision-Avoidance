079015fbf0e1866270c1b90d6a8df355ed398782
LiqiangHou <68376937+LiqiangHou@users.noreply.github.com>
2024-04-30 22:16:36 -0700

    Update plot_trajectory_and_thrust_profile.m

--- plot_trajectory_and_thrust_profile.m
+++ plot_trajectory_and_thrust_profile.m
@@ -1,16 +1,15 @@
-function plot_trajectory_and_thrust_profile
+function plot_trajectory_and_thrust_profile_git
 
 clear all;
 close all;
 global self
 global MATRIX_A
 
+
+
 format long g;
 
-
 MATRIX_A = eye(7 , 7)*1.0e-12;
-
-
 
 % constant paramters
 dayszero = 0.15;    % p=0.10;
@@ -23,21 +22,15 @@ self.tau = [0.0 , 1.0];
 
 
 % the solution
-% feq = [     0.00518461498391793      -0.00136506947682165       0.00746618131565968       -0.0100697825790903         0.244086808814658         0.960638646307336       0.441788421772173]
-xoptm = [0.0383921584146486	0.00411304099871005	0.223911689663605	-0.448626642563718	-0.00233822057439055	0.441811560481595	0.0375187731455364];
-
-
-% feq = [    0.00910619305097955      -0.00656041928460005
-% 0       0.00544582565864205         0.692974539167654
-% 0.912832126423306          0.29898300627914];
-% ode113 option:RfEvents
 xoptm = [0.0990177915105663       0.00917255395250577        0.0887312321784412        -0.103009615889113      -0.00614950306603373         0.298989623495269         0.118144838982202];
+
+
 % settings of ode
 tspan = [0, 1];
-
-% 
 tau_0 = tspan(1);
 tau_f = tspan(2);
+
+%
 plot_fitness(tau_0,tau_f,xoptm);
 
 
@@ -60,7 +53,7 @@ global m1
 global days
 global max_delta_alpha
 
-%State vecotr X has components
+%State vecotr components
 %x(1)=rbar, nondimensionalized radius from center of earth
 %x(2)=theta
 %x(3)=alfa
@@ -74,10 +67,11 @@ global max_delta_alpha
 %x(11)=lambda_wbar
 %x(12)=mass
 %x(13)=lambda_mass
+%x(14)=collison_prob
 
 % initial value of state and costate of the ode, 
 % x0: optimal initial value of cosctate
-y0   = [r0bar theta0 alfa0 u0bar v0bar w0bar x0(1) x0(2) x0(3) x0(4) x0(5) m1 x0(6)];
+y0   = [r0bar theta0 alfa0 u0bar v0bar w0bar x0(1) x0(2) x0(3) x0(4) x0(5) m1 x0(6) 0.0];
 days = x0(7);
 
 % Initial amount of days to thrust. 
@@ -96,7 +90,7 @@ rtol = 1.0e-6;
 % options = odeset('RelTol',rtol,'AbsTol',atol,'Events',[]);
 options = odeset('RelTol',rtol,'AbsTol',atol,'Events',@RfEvents);
 %              
-sol  = ode113(@odes_function_flux, tspan, y0,options);
+sol  = ode113(@odes_function_flux_prob, tspan, y0,options);
 
 
 
@@ -104,7 +98,7 @@ Nt      = 10000;
 tau_end = sol.xe;
 tau     = linspace(0,tau_end,Nt);
 
-plot_results(sol,tau);
+plot_results(sol,tau,days);
 
 
 return
@@ -208,57 +202,6 @@ end
 
 
 
-function J = Obj_Convolution(x)
-
-
-
-% initial value of costate and days
-xzero  = x(1:7);                
-
-
-% initial variance of the final condition
-
-tau_0 = 0.0;
-tau_f = 1.0;      % tau_f should be keep unvaried
-[feq]  = fitness(tau_0,tau_f,xzero);
-Mu    = feq;
-
-
-
-% SIGMA =  Jacobian_Covariance(tau_0,tau_f,xzero);
-% 
-          
-% SIGMA: covraiance of initila guess  uisng Jacobian 
-% 
-% % % p=0.1N, sucess,deg45
-SIGMA =[...
-        0.0335873890035292        0.0123059851447506       -0.0224488706419929       0.00435895276899434        0.0255799397700252        0.0255799416027115        0.0528380895221596
-        0.0123059851447506       0.00450875387684673      -0.00822497600536212       0.00159706394612208       0.00937215925835937       0.00937215992983193         0.019359192959845
-       -0.0224488706419929      -0.00822497600536212        0.0150041967551564      -0.00291340201631715       -0.0170969157163996       -0.0170969169413158       -0.0353155000088981
-       0.00435895276899434       0.00159706394612208      -0.00291340201631715      0.000565702479592174       0.00331975043608015       0.00331975067392516       0.00685729803548551
-        0.0255799397700252       0.00937215925835937       -0.0170969157163996       0.00331975043608015        0.0194815178568766        0.0194815192526387        0.0402411496588206
-        0.0255799416027115       0.00937215992983193       -0.0170969169413158       0.00331975067392516        0.0194815192526387        0.0194815206484009        0.0402411525419158
-        0.0528380895221596         0.019359192959845       -0.0353155000088981       0.00685729803548551        0.0402411496588206        0.0402411525419158        0.0831223797735037
-        ];
-% % % p=0.1N, sucess!!!
-s =         [0.01 0.01 0.001 0.001 0.01 0.0005 0.001]*5.0e1   ;    ...  %        variance of boundary  sucess !!!
-
-
-
-S = diag(s);
-
-% performance index
-M = zeros(1,7);
-J = quadratic_convolution(Mu,M,SIGMA,S);
-
-
-return
-end
-
-
-
-
-
 
 
 
@@ -274,12 +217,16 @@ end
 
 
 
-function plot_results(sol,tau)
+function plot_results(sol,tau,days)
 
 global v0 u0 l l1 tf m12 m m2 p m1 r0 mu r0bar u0bar v0bar theta0 alfa0 w0bar ufbar lambdaf_ubar ...
 lambdaf_alfa alfaf lambdaf_wbar wfbar
 
 global period
+global fitflux  fitfgrad
+
+global area_c
+
 
 
 % result structure of the ODE
@@ -297,13 +244,14 @@ lambda_alfa_sol = Z(8,:);
 lambda_ubar_sol = Z(9,:);
 lambda_vbar_sol = Z(10,:);
 lambda_wbar_sol = Z(11,:);
+collision_prob  = Z(14,:);
 
 %%plot and results
 final_radius = r_sol(end)
 final_theta = theta_sol(end);
 X = r_sol.*cos(theta_sol);
 Y = r_sol.*sin(theta_sol);
-time = tau*(tf);
+time = tau*(tf);       % tf equals days*86400
 
 beta = atand((lambda_ubar_sol./r0 +...
 lambda_wbar_sol.*m2.*sin(alfa_sol)./(m12.*l))./(lambda_wbar_sol.*m2.*cos(alfa_sol)./(m12*l)...
@@ -327,30 +275,66 @@ plot(final_radius*cos(final_theta),final_radius*sin(final_theta),'-.ro','MarkerS
 % figure(3)
 % plot(X1,Y1)
 figure(2)
-plot(time/period,beta1,'k')
-xlabel('time [number of inital period]','fontsize',14)
+plot(time,beta1,'k')
+xlabel('time [sec]','fontsize',14)
 ylabel('Thrust Angle [deg]','fontsize',14)
 title('Thrust Angle Vs Time','fontsize',16)
 figure(3)
-plot(time/period,A*180/pi,'k')
-xlabel('time [number of inital period]','fontsize',14)
+plot(time,A*180/pi,'k')
+xlabel('time [sec]','fontsize',14)
 ylabel('Tether Angle [deg]','fontsize',14)
 title('Tether Angle Vs Time','fontsize',16)
 figure(4)
-plot(time/period,beta,'k')
-xlabel('time [number of inital period]','fontsize',14)
+plot(time,beta,'k')
+xlabel('time [sec]','fontsize',14)
 ylabel('Thrust Angle [deg]','fontsize',14)
 title('Thrust Angle Vs Time','fontsize',16)
 tf
 
-figure(5)
+figure(4)
 alt = r_sol - 6378.0;
 
-plot(time/period,alt,'k')
-xlabel('time [number of inital period]','fontsize',14)
+plot(time,alt,'k')
+xlabel('time [sec]','fontsize',14)
 ylabel('Altitude [km]','fontsize',14)
 title('Altitude Vs Time','fontsize',16)
-tf
+
+
+% flux data
+
+
+r_data = Z(1,:);
+
+for i = length(r_data)
+
+    [flux,flux_partial]=flux_valueandpartial_2d(r_data(i),fitflux, fitfgrad) ;
+    flux_data(i) = flux;
+
+end
+
+% plot collison
+figure(5)
+plot(time,collision_prob,'k')
+xlabel('time [sec]','fontsize',14)
+ylabel('','fontsize',14)
+title('Accumulated Collision Probablity Vs Time','fontsize',16)
+
+
+
+beta_set_rad = deg2rad(beta);
+x_set        = Z;
+[p_history,rho_histoy] = Thrust_profile(time,x_set,beta_set_rad);
+
+
+figure(6)
+% Thrust vs. time
+
+
+plot(time,p_history,'k')
+xlabel('time [sec]','fontsize',14)
+ylabel('Thrust(N) ','fontsize',14)
+title('Thrust Vs Time','fontsize',16)
+ylim([-0.001,0.11]);
 
 
 
@@ -360,4 +344,82 @@ return
 end
 
 
+function  [p_history,rho_histoy] = Thrust_profile(time,x_set,beta_set_rad)
+global tf_days t0_days area_c v_threshold delta_t tf v0 p mu m12 l m m2 r0
+
+
+
+for i=1:length(time)
+    x        = x_set(:,i);
+    beta_rad = beta_set_rad(i);
+
+    rho = switch_function(x,beta_rad);
+    if(rho > 0)
+        const_p = p;
+        p = 0;
+    else
+        p = p;
+    end
+
+    p_history(i)  = p;
+    rho_histoy(i) = rho;
+
+
+    % 
+    % 
+    if(rho > 0)
+        % restore the constant value of p
+        p = const_p;
+    else
+        p = p;
+    end
+end
+return
+end
+
+function rho = switch_function(x,beta)
+global flux flux_partial tf_days t0_days area_c v_threshold delta_t tf v0 p mu m12 l m m2 r0
+global ISP
+global v0 u0 l l1 tf m12 m m2 p m1 r0 mu r0bar u0bar v0bar theta0 alfa0 w0bar ufbar lambdaf_ubar ...
+lambdaf_alfa alfaf lambdaf_wbar wfbar
+
+
+rbar = x(1); %nondimensionalized radius from center of earth
+theta= x(2) ;
+alfa= x(3)  ;
+ubar= x(4) ;
+vbar= x(5) ;
+wbar= x(6) ;
+lambda_rbar = x(7) ;
+lambda_alfa = x(8);
+lambda_ubar = x(9);
+lambda_vbar= x(10);
+lambda_wbar= x(11);
+mass       = x(12) ;
+lambda_mass= x(13) ;
+
+
+
+% rho= (lambda_ubar / r_0) *(sin(beta) / m)  ...
+%     - lambda_vbar * cos(beta)/ (m * r0 * rbar) ...
+%     + lambda_wbar * ((m2 * cos(beta-alpha))/(m * m12 * l) + cos(beta) / (m * r0 * rbar))
+%     - lambda_mass * (v0 /Isp);
+
+
+
+rho= x(9)/r0*sin(beta)/m             ...
+   - x(10)* cos(beta)/(m*r0*x(1))  ...
+   + x(11)* (m2*cos(beta-x(3))/(m*m12*l) + cos(beta)/(m*r0*x(1))) ...
+   - x(13) * (v0 /ISP);
+
+
+
+
+
+
+
+
+return
+end
+ 
 
